fa0462e6c98e007e6ad93e767e4f613af120acc1
tysong94 <tysong94@oniontech.com>
2020-06-11 17:29:18 +0900

    글자색 바꾸기

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#3498db";
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    const currentColor = title.style.color;
-    if(currentColor === BASE_COLOR) {
-        title.style.color = OTHER_COLOR;
+    const currentClass = title.className;
+    if(currentClass !== CLICKED_CLASS) {
+        title.className = CLICKED_CLASS;
     } else {
-        title.style.color = BASE_COLOR;
+        title.className = "";
     }
 }
 
 function init() {
-    title.style.color = BASE_COLOR;
-    title.addEventListener("mouseenter", handleClick);
+    title.addEventListener("click", handleClick);
 }
 init();
