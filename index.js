938f3304c2f50fe808ef15a07b9d418c4e31d71f
tysong94 <tysong94@oniontech.com>
2020-06-11 17:37:54 +0900

    toggle

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    const hasClass = title.classList.contains(CLICKED_CLASS); // true or false 반환
+    //  아래랑 똑같은 기능을 한다.
+    title.classList.toggle(CLICKED_CLASS);    
+    
+    // const hasClass = title.classList.contains(CLICKED_CLASS); // true or false 반환
 
-    if(hasClass) {
-        title.classList.remove(CLICKED_CLASS);
-    } else {
-        title.classList.add(CLICKED_CLASS);
-    }
+    // if(hasClass) {
+    //     title.classList.remove(CLICKED_CLASS);
+    // } else {
+    //     title.classList.add(CLICKED_CLASS);
+    // }
 }
 
 function init() {
