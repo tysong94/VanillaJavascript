5cd7d8a451247206fecadab273b314620d0d0959
tysong94 <tysong94@oniontech.com>
2020-06-11 17:35:40 +0900

    class add, remove

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    const currentClass = title.className;
-    if(currentClass !== CLICKED_CLASS) {
-        title.className = CLICKED_CLASS;
+    const hasClass = title.classList.contains(CLICKED_CLASS); // true or false 반환
+
+    if(hasClass) {
+        title.classList.remove(CLICKED_CLASS);
     } else {
-        title.className = "";
+        title.classList.add(CLICKED_CLASS);
     }
 }
 
