9f5bcb90a7a5028d5b0e853b2f554e6b8db0f337
tysong94 <tysong94@oniontech.com>
2020-06-15 10:45:25 +0900

    #3.4 Saving the User Name part Two

--- greeting.js
+++ greeting.js
@@ -5,24 +5,43 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser", 
     SHOWING_CN = "showing";
 
-// form을 없애고 h4를 보여준다.
-function paintGreeting(text) { 
-    form.classList.remove(SHOWING_CN);
-    greetings.classList.add(SHOWING_CN);
-    greetings.innerText = `Hello ${text}`;
-}
-
-function loadName() {
-    const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null) { 
-
-    } else  {
-        paintGreeting(currentUser);
-    }
-}
+init();
 
 function init() {
     loadName();
 }
 
-init();
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null) { 
+        askForName();
+    } else  {
+        paintGreeting(currentUser);
+    }
+}
+
+// currentUser가 없을 때 from을 보여준다.
+function askForName() { 
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+// form 서브밋시 동작
+function handleSubmit(event) {
+    event.preventDefault(); // 기본동작을 막음. 페이지가 새로고침되고 어딘가로 보내지는...
+    const currentValue = input.value;
+    saveName(currentValue);
+    paintGreeting(currentValue);
+}
+
+// 로컬 스토리지에 키, 밸루로 저장.
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+// currentUser가 있을 때 form을 없애고 h4를 보여준다.
+function paintGreeting(text) { 
+    form.classList.remove(SHOWING_CN);
+    greetings.classList.add(SHOWING_CN);
+    greetings.innerText = `Hello ${text}`;
+}
